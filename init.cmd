bc419aaaafceff1b8f40226d7936e4730bd60b05
tenshi <sairi.na.tenshi@gmail.com>
2010-10-17 20:36:52 +0400

    master

--- init.cmd
+++ init.cmd
@@ -1,1 +1,1 @@
-git init && git pull git://github.com/sairi-na-tenshi/wc_std-hlight.git && git submodule init && git submodule update
+git init && git pull git://github.com/sairi-na-tenshi/wc_std-hlight.git master && git submodule init && git submodule update
